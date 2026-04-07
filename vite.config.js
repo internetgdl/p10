709a090ea6dca482f9e57beb6d3f630f034bdbf0
Luis Eduardo Estrada <eduardo@eduardo.mx>
2026-04-07 00:14:25 -0600

    prueba

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  build: {
-    outDir: 'build' // Aquí nos aseguramos de que el output sea en la carpeta build según lo pedido
-  }
-});
